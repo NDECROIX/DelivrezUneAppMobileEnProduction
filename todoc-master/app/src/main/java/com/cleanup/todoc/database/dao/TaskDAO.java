33d74ef5e8e424da74f93ad1208eb909be913c8c
NDECROIX <nicolas.decroixx@gmail.com>
2019-08-11 18:04:27 +0200

    Create ProjectDAO and TaskDAO

---
+++ todoc-master/app/src/main/java/com/cleanup/todoc/database/dao/TaskDAO.java
@@ -0,0 +1,31 @@
+package com.cleanup.todoc.database.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
+
+@Dao
+public interface TaskDAO {
+
+    @Query("SELECT * FROM Task WHERE projectId = :projectId")
+    LiveData<List<Task>> getTasksByProject(long projectId);
+
+    @Query("SELECT * FROM Task")
+    LiveData<List<Task>> getTasks();
+
+    @Insert
+    void insertTask(Task task);
+
+    @Update
+    void updateTask(Task task);
+
+    @Delete
+    void deleteTask(Task task);
+}
